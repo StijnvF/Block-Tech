8fe92c9a7f2888e002ec7ab26319587278754327
Stijnvf <stijnvf1@gmail.com>
2021-04-22 12:16:58 +0200

    404 page

--- server.js
+++ server.js
@@ -10,6 +10,15 @@ app.get('/register', (req, res) => {
   res.send('This will become the register page.')
 });
 
+app.get('/login', (req, res) => {
+  res.send('This will become the login page.')
+});
+
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry can't find that!")
+})
+
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
