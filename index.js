ef5f617e7fa9df34f23537692ee4e473f592490b
Stijnvf <stijnvf1@gmail.com>
2021-05-09 22:41:03 +0200

    start posten

--- index.js
+++ index.js
@@ -2,25 +2,25 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.set('view engine', 'ejs');
 
 app.use(express.static('public'))
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded());
+
 
 const users = [{
-    username: 'Victor',
-    age: '22',
-    city: 'Amsterdam'
+    firstname: 'Victor',
+    lastname: 'Boucher',
+    age: '01-01-1998',
+    keuken: 'japans'
   },
   {
-    username: 'Stijn',
-    age: '23',
-    city: 'Purmerend'
+    firstname: 'Stijn',
+    lastname: 'van Fraeijenhove',
+    age: '24-02-1998',
+    keuken: 'italiaans'
   },
-  {
-    username: 'Peter',
-    age: '31',
-    city: 'Utercht'
-  }
 ]
 
 app.get('/', (req, res) => {
@@ -32,8 +32,17 @@ app.get('/profile', (req, res) => {
 });
 
 app.get('/create', (req, res) => {
-  res.render('create', {foo: 'FOO'});
+  res.render('create', {title: 'create profile'});
 });
+
+app.post('/create', (req, res) => {
+  // console.log(req.body)
+  let profile ={firstname: req.body.firstname, lastname: req.body.lastname, leeftijd: req.body.leeftijd, keuken: req.body.keuken}
+  users.push(profile)
+  res.render('index', {title: 'succesfully added profile' , users});
+});
+
+
 
 app.use(function (req, res, next) {
   res.status(404).send("Sorry can't find that!")
