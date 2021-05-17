3d8851fb0127f9ec589ae1f67ec2a4b0b9d57499
Stijnvf <stijnvf1@gmail.com>
2021-05-17 21:19:55 +0200

    added comments

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded());
 
-
+// tijdelijke opslag variabelen, todat ik een mongoDB connectie heb.
 const users = [{
   firstname: 'Stijn',
   lastname: 'van Fraeijenhove',
@@ -16,13 +16,13 @@ const users = [{
   keuken: 'italiaans'
   },
   // {
-  //   firstname: 'Victor',
-  //   lastname: 'Boucher',
-  //   leeftijd: '01-01-1998',
+  //   firstname: 'Tom',
+  //   lastname: 'Kool',
+  //   leeftijd: '21-08-1997',
   //   keuken: 'japans'
   // },
 ]
-
+//laden van de pagina's
 app.get('/', (req, res) => {
   res.render('index', {users});
 });
@@ -39,14 +39,15 @@ app.get('/edit', (req, res) => {
   res.render('edit', {title: 'edit profile'});
 });
 
-app.post('/create', (req, res) => {
+//uitvoeren van de form en doorsturen naar profile.
+app.post('/toevoegen', (req, res) => {
   // console.log(req.body)
   let profile ={firstname: req.body.firstname, lastname: req.body.lastname, leeftijd: req.body.leeftijd, keuken: req.body.keuken}
   users.push(profile)
   res.render('profile', {title: 'succesfully added profile' , users});
 });
 
-
+//error message
 app.use(function (req, res, next) {
   res.status(404).send("Sorry can't find that!")
 })
